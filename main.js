c5ea256277e96b31745aab0826f7f75ad80c6564
Michał Śnieżko <michal.sniezko@softwaremind.com>
2019-11-29 12:35:54 +0100

    After episode 8 components in components

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 Vue.component('task', {
     template: '<li><slot></slot></li>',
+});
+
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.task }}</task><div>',
     data() {
         return {
-            message: 'FooBar message from component'
+            tasks: [
+                { task: 'Go to the store', complete: true },
+                { task: 'Go to the email', complete: true },
+                { task: 'Go to the farm', complete: true },
+                { task: 'Go to the work', complete: true }
+            ]
         }
     }
 });
